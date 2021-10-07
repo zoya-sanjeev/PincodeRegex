626b1d43e3911ed6fffb973c10d8e088b947e7a0
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-10-07 22:07:14 +0530

    ADDED: function to validate whether pincode has 6 digits or not

--- PincodeValidation.js
+++ PincodeValidation.js
@@ -1,1 +1,17 @@
 console.log("Welcome to pincode validation");
+
+let pinCodeRegex = RegExp("^[0-9]{6}");
+
+function validatePincode(pincode){
+    if(pinCodeRegex.test(pincode))
+        console.log("Valid Pincode");
+    else
+        throw 'Invalid pincode';
+}
+
+try{
+    validatePincode("500000");
+    validatePincode("8989");
+}catch(e){
+    console.error(e);
+}
